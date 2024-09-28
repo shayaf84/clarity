41e4a10940124cc9e54df79100733a30f0ea1bad
xacer <chacecaven@outlook.com>
2024-09-28 09:14:33 -0400

    test webrtc video streaming

--- src/camera.js
+++ src/camera.js
@@ -1,2 +1,13 @@
 
 /* todo: load camera stream, downsample in both spatial and temporal axes */
+
+/* main idea: capture video frames from video element and send to server along with audio */
+
+async function getMedia() {
+
+    let stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: { width: 1280, height: 720 }
+    });
+
+}
